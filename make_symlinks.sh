90a2ec40e4d3f8bd551d7b543d5884663f29d612
David Hatch <dhatch@rayzist.com>
2014-07-08 23:27:35 -0700

    adds my list of files and fixes syntax slightly

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -8,26 +8,26 @@
 
 dir=~/dotfiles # dotfiles directory
 olddir=~/dotfiles_old # old dotfiles backup directory
-files="bashrc vimrc vim" # list of files/folders to symlink in homedir
+files="bashrc bash_profile vimrc vim tmux.conf" # list of files/folders to symlink in homedir
 
 ##########
 
 # create dotfiles_old in homedir
 echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
+mkdir -p "$olddir"
 echo "done"
 
 # change to the dotfiles directory
 echo -n "Changing to the $dir directory ..."
-cd $dir
+cd "$dir"
 echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    mv ~/."$file" "$olddir/"
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    ln -s "$dir/$file" ~/."$file"
 done
 
 install_zsh () {
